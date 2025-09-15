d52153559f15013991b2cab2e366b447c37a280f
abeatrizdscoelho <anabeatrizdscoelho@gmail.com>
2025-09-15 19:46:19 -0300

    feat: Criado página de cadastro

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,10 +6,13 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  passwordConfirm: FormControl
 }
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -21,31 +24,33 @@ interface LoginForm {
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup
+export class SignupComponent {
+  signupForm!: FormGroup
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]) //Nenhuma senha menor do que 6 caracteres.
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]), //Nenhuma senha menor do que 6 caracteres.
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
 
   submit() {
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
       next: () => this.toastService.success("Login feito com sucesso!"),
       error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde.")
     })
   }
 
   navigate() {
-    this.router.navigate(["/signup"])
+    this.router.navigate(["/login"])
   }
 
 }
